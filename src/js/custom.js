196dacdb51278e863e0fc2aabc6b52f80a174cea
me <admin@admin.com>
2020-04-14 13:19:06 +0500

    Add expand block

--- src/js/custom.js
+++ src/js/custom.js
@@ -9,7 +9,14 @@ let closePopup = document.querySelector(".popup__close");
 let mainNav = document.querySelector(".main-nav");
 let mainNavToggle = document.querySelector(".main-nav__toggle");
 
-
+let expandBlock = document.querySelector(".expand");
+expandBlock.addEventListener("click", function() {
+    let toggle = event.target.closest(".expand__toggle");
+    if (toggle) {
+        let expandItem = toggle.closest(".expand__item");
+        expandItem.classList.toggle("expand__item--opened");
+    }
+});
 
 mainNavToggle.addEventListener('click', function(evt) {
     evt.preventDefault();
